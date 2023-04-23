8a58a7c9dd482f929f00c9db1bccfdcceb59fcb1
Erik Egnatosyan <egnatosyan.sans@mail.ru>
2023-04-23 21:33:45 +0400

    Добавьте файлы проекта.

---
+++ TestMVCInPractice/Controllers/ContactsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestMVCInPractice.Models;
+
+namespace TestMVCInPractice.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index() //Contacts
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Check(Contact contact)
+        {
+            if(ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ TestMVCInPractice/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestMVCInPractice.Models
+{
+    public class Contact
+    {
+        [Display(Name = "Введите имя")]
+        [Required(ErrorMessage = "Вам нужно ввсети имя")]
+        public string Name { get; set; }
+
+        [Display(Name = "Введите фамилию")]
+        [Required(ErrorMessage = "Вам нужно ввсети фамилию")]
+        public string SurName { get; set; }
+
+        [Display(Name = "Введите возраст")]
+        [Required(ErrorMessage = "Вам нужно ввсети возраст")]
+        public int Age { get; set; }
+
+        [Display(Name = "Введите почту")]
+        [Required(ErrorMessage = "Вам нужно ввсети почту")]
+        public string Email { get; set; }
+
+        [Display(Name = "Введите сообщение")]
+        [Required(ErrorMessage = "Вам нужно ввсети сообщение")]
+        [StringLength(30, ErrorMessage = "Текст менее 30 символов")]
+        public string Message { get; set; }
+    }
+}
